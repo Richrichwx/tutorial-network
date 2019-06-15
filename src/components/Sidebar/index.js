3828553c6eddce1bd2248761b3c9c7af9a0171a3
Anna <guseva.annawx@gmail.com>
2019-06-15 13:19:33 +0300

    add project

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <div className='Sidebar'>Sidebar</div>
+    )
+  }
+}
+
+export default Sidebar;
