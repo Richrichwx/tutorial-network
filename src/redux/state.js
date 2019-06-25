dde85e75d96e1b9e89aff4e652c34297abfb973f
Anna <guseva.annawx@gmail.com>
2019-06-25 15:12:46 +0300

    fix

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,77 +1,77 @@
-let rerenderEntreTree = ()  => {
-  console.log('state')
-};
-
-let state = {
-  profilePage: {
-    posts: [
-      {
-        id: 1,
-        text: 'Хорошая погода',
-        like: '0'
-      },
-      {
-        id: 2,
-        text: 'Плохая погода',
-        like: '23'
-      },
-    ],
-    newPostText: ''
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        {
+          id: 1,
+          text: 'Хорошая погода',
+          like: '0'
+        },
+        {
+          id: 2,
+          text: 'Плохая погода',
+          like: '23'
+        },
+      ],
+      newPostText: ''
+    },
+    dialogsPage: {
+      dialogs: [
+        {
+          id: 1,
+          name: 'Дмитрий'
+        },
+        {
+          id: 2,
+          name: 'Мария'
+        }, {
+          id: 3,
+          name: 'Анна'
+        },
+        {
+          id: 4,
+          name: 'Александр'
+        }, {
+          id: 5,
+          name: 'Максим'
+        },
+      ],
+      messages: [
+        {
+          id: 1,
+          text: 'Привет'
+        },
+        {
+          id: 2,
+          text: 'Как ты?'
+        }
+      ],
+    }
   },
-  dialogsPage: {
-    dialogs: [
-      {
-        id: 1,
-        name: 'Дмитрий'
-      },
-      {
-        id: 2,
-        name: 'Мария'
-      }, {
-        id: 3,
-        name: 'Анна'
-      },
-      {
-        id: 4,
-        name: 'Александр'
-      }, {
-        id: 5,
-        name: 'Максим'
-      },
-    ],
-    messages: [
-      {
-        id: 1,
-        text: 'Привет'
-      },
-      {
-        id: 2,
-        text: 'Как ты?'
-      }
-    ],
+  getState() {
+    return this._state
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      text: this._state.profilePage.newPostText,
+      like: 0
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText =  '';
+    this._callSubscribe( this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscribe(this._state);
+    console.log(newText)
+  },
+  subscribe(observer) {
+    this._callSubscribe = observer;
+  },
+  _callSubscribe () {
+    console.log('state')
   }
 };
-window.state =  state;
 
- export const addPost = () => {
-  let newPost = {
-    id: 5,
-    text: state.profilePage.newPostText,
-    like: 0
-  };
-  state.profilePage.posts.push(newPost);
-   state.profilePage.newPostText =  '';
-   rerenderEntreTree(state);
-};
-
-export const updateNewPostText = newText => {
-  state.profilePage.newPostText = newText;
-  rerenderEntreTree(state);
-  console.log(newText)
-};
-
-export const subscribe = (observer) => {
-  rerenderEntreTree = observer;
-};
-
-export default state;
+export default store;
