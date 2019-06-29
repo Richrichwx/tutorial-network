f1182a8f973e36fc513ec15d6574a527c3acc3b5
Anna <guseva.annawx@gmail.com>
2019-06-29 22:26:51 +0300

    fix

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -2,14 +2,12 @@ const dialogsReducer = (state,action) => {
   if (action.type === 'ADD_MESSAGE') {
     let newMessage = {
       id: 3,
-      text: this._state.dialogsPage.newMessageText,
+      text: state.newMessageText,
     };
-    this._state.dialogsPage.messages.push(newMessage);
-    this._state.dialogsPage.newMessageText = '';
-    this._callSubscribe(this._state);
+    state.messages.push(newMessage);
+    state.newMessageText = '';
   } else if (action.type === 'UPDATE_NEW_MESSAGE_TEXT') {
-    this._state.dialogsPage.newMessageText = action.newMessage;
-    this._callSubscribe(this._state);
+    state.newMessageText = action.newMessage;
   }
   return state;
 };
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -2,15 +2,13 @@ const profileReducer = (state,action) => {
   if (action.type === 'ADD_POST') {
     let newPost = {
       id: 5,
-      text: this._state.profilePage.newPostText,
+      text: state.newPostText,
       like: 0
     };
-    this._state.profilePage.posts.push(newPost);
-    this._state.profilePage.newPostText = '';
-    this._callSubscribe(this._state);
+    state.posts.push(newPost);
+    state.newPostText = '';
   } else if (action.type === 'UPDATE_NEW_POST_TEXT') {
-    this._state.profilePage.newPostText = action.newText;
-    this._callSubscribe(this._state);
+    state.newPostText = action.newText;
   }
   return state;
 };
