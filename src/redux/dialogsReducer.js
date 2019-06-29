e70c90548c654b12aa2be4cc1539dff4eafaae65
Anna <guseva.annawx@gmail.com>
2019-06-29 22:20:33 +0300

    fix

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -1,4 +1,8 @@
 const dialogsReducer = (state,action) => {
-
+  if (action.type === 'ADD_MESSAGE') {
+    this._addMessage();
+  } else if (action.type === 'UPDATE_NEW_MESSAGE_TEXT') {
+    this._updateNewMessageText(action.newMessage)
+  }
   return state;
 };
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,4 +1,8 @@
 const profileReducer = (state,action) => {
-
+  if (action.type === 'ADD_POST') {
+    this._addPost();
+  } else if (action.type === 'UPDATE_NEW_POST_TEXT') {
+    this._updateNewPostText(action.newText)
+  }
   return state;
 };
