47cdce73d806b724b6bcac6d0566bf5f90c9faff
Anna <guseva.annawx@gmail.com>
2019-07-09 21:22:57 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 
 let rerenderEntreTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
-      <App state={ state } dispatch={ store.dispatch.bind(store) } />
-
+      <Provider store={store}>
+        <App state={ state } dispatch={ store.dispatch.bind(store) } />
+      </Provider>
     </BrowserRouter>, document.getElementById('root'));
 };
 rerenderEntreTree(store.getState());
