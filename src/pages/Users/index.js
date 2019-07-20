73ac30739051ccc7517a2eab298659fd5f0f45ed
Anna <guseva.annawx@gmail.com>
2019-07-20 15:39:32 +0300

    fix

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -3,12 +3,15 @@ import styled from 'styled-components';
 
 const UsersContainer = styled.div`
   width: 800px;
+  background: #c2a7a1;
+  height: 460px;
+  padding: 20px 0;
 `;
 
 class Users extends React.Component {
   render() {
     return (
-      <UsersContainer>Музыка</UsersContainer>
+      <UsersContainer>Пользователи</UsersContainer>
     )
   }
 }
