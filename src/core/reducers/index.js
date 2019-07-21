fa7bdfd97bdb0e29e86f348e75cfd42d1a004fad
Anna <guseva.annawx@gmail.com>
2019-07-21 15:47:58 +0300

    add reducers

--- src/core/reducers/index.js
+++ src/core/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import messages from '../../store/messages';
 import profile from '../../store/profile';
-import nameDialogs from '../../store/nameDialogs'
+import nameDialogs from '../../store/nameDialogs';
+import users from '../../store/users'
 
 export default combineReducers({
   profile,
   messages,
-  nameDialogs
+  nameDialogs,
+  users
 })
--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 
 const UsersContainer = styled.div`
   width: 800px;
@@ -9,9 +10,18 @@ const UsersContainer = styled.div`
 `;
 
 const Users = () => {
+  debugger
   return (
     <UsersContainer>Пользователи</UsersContainer>
   )
 };
 
-export default Users;
+const mapStateToProps = state => ({
+  users: state.users
+});
+
+const mapDispatchToProps = {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
