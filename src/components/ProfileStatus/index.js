3d10d06c07c7aba26a1b6559817527838ab0bee6
Anna <guseva.annawx@gmail.com>
2019-08-07 15:02:31 +0300

    add status

--- src/components/ProfileStatus/index.js
+++ src/components/ProfileStatus/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
 
-class ProfileStatus extends  React.Component {
+class ProfileStatus extends React.Component {
+  state = {
+    status: 'Hello my  friends',
+    editMode: false
+  };
+
+  activatedAditMode = () => {
+    this.setState({
+      editMode: true
+    })
+  };
+  diactivatedAditMode = () => {
+    this.setState({
+      editMode: false
+    })
+  };
+
   render() {
     return (
       <div>
-        <input type="text"/>
+        { !this.state.editMode ? (
+          <div onClick={ this.activatedAditMode }>{ this.state.status }</div>
+        ) : (
+          <input type="text" value={ this.state.status }
+                 onBlur={ this.diactivatedAditMode }
+                 autoFocus={ true }/>
+        ) }
       </div>
     )
   }
