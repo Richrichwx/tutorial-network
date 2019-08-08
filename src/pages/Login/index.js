59b4cef4502deccce88f0b7426db7a450a836677
Anna <guseva.annawx@gmail.com>
2019-08-08 23:30:58 +0300

    add form

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,12 +3,30 @@ import styled from 'styled-components';
 
 const MusicContainer = styled.div`
   width: 800px;
+  background: #c2a7a1;
+  padding: 20px 0;
 `;
 
 class Login extends React.Component {
   render() {
     return (
-      <MusicContainer>Login</MusicContainer>
+      <MusicContainer>
+        <h1>Login</h1>
+        <form>
+          <div>
+            <input type="text" placeholder="Login"/>
+          </div>
+          <div>
+            <input type="text" placeholder="Password"/>
+          </div>
+          <div>
+            <input type="checkbox"/>
+          </div>
+          <div>
+            <button>Login</button>
+          </div>
+        </form>
+      </MusicContainer>
     )
   }
 }
