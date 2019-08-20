7188e41070da8303c28565e73da48146183c0b82
Anna <guseva.annawx@gmail.com>
2019-08-21 00:25:56 +0300

    hooks useEffect

--- src/components/ProfileStatus/index.js
+++ src/components/ProfileStatus/index.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ProfileStatus =(props) =>  {
   const [editMode,setEditMode] = useState(false);
   const [status,setStatus] = useState(props.status);
+
+  useEffect( () => {
+    setStatus(props.status)
+  }, [props.status]);
+  
   const activationEdit = () => {
     setEditMode(true);
   };
