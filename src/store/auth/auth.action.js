c964416458668f9c438116efaf6e3ac9e52a11d7
Anna <guseva.annawx@gmail.com>
2019-09-02 23:56:44 +0200

    destructuring

--- src/store/auth/auth.action.js
+++ src/store/auth/auth.action.js
@@ -1,7 +1,7 @@
 import { stopSubmit } from 'redux-form'
 import api from './auth.api';
 
-export const setAuthData = (id, email, login,isAuth) => ({
+export const setAuthData = (id, email, login, isAuth) => ({
   type: 'SET_AUTH_DATA',
   data: {
     id,
@@ -11,42 +11,39 @@ export const setAuthData = (id, email, login,isAuth) => ({
   },
 });
 
-export const setAuthDataThunk = () => {
-  return (dispatch) => {
-    return api.getAuth()
-      .then(response => {
-        if (response.data.resultCode === 0) {
-          let { id, email, login } = response.data.data;
-          dispatch(setAuthData(id, email, login, true))
-        }
-      })
+export const setAuthDataThunk = () => async (dispatch) => {
+  const { data: {data} } = await api.getAuth();
+  if (data.resultCode === 0) {
+    let { id, email, login } = data.data;
+    dispatch(setAuthData(id, email, login, true))
   }
+  console.log(data)
 };
 
 export const loginThunk = (email, password, rememberMe) => {
   return (dispatch) => {
-    api.login( email, password,rememberMe )
-      .then(response => {
-        if (response.data.resultCode === 0) {
-          dispatch(setAuthDataThunk())
-        } else {
-          let messages = response.data.messages.length > 0 ? response.data.messages[0] : 'some error';
-          dispatch(stopSubmit("login", {
-            _error: messages
-          }))
-        }
-      })
+    api.login(email, password, rememberMe)
+       .then(response => {
+         if (response.data.resultCode === 0) {
+           dispatch(setAuthDataThunk())
+         } else {
+           let messages = response.data.messages.length > 0 ? response.data.messages[0] : 'some error';
+           dispatch(stopSubmit('login', {
+             _error: messages
+           }))
+         }
+       })
   }
 };
 
 export const logOutThunk = () => {
   return (dispatch) => {
     api.LogOut()
-      .then(response => {
-        if (response.data.resultCode === 0) {
-          dispatch(setAuthData(null,null,null,false))
-        }
-      })
+       .then(response => {
+         if (response.data.resultCode === 0) {
+           dispatch(setAuthData(null, null, null, false))
+         }
+       })
   }
 };
 
